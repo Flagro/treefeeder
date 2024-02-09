3d3ecccb004db5e1ed38b5cdf4ffba1ee2bab4bd
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-02-09 17:56:39 +0100

    Add treefeeder script and CLI module

---
+++ treefeeder/cli.py
@@ -0,0 +1,9 @@
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='List contents of directories in a tree-like format.')
+    parser.add_argument('directory', nargs='?', default='.', help='Directory to start listing')
+    parser.add_argument('-P', '--pattern', action='append', help='List only those files that match the pattern')
+    parser.add_argument('-I', '--ignore', action='append', help='Do not list files that match the given pattern')
+    return parser.parse_args()
