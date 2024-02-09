fa27207f1be998497da3e74cd018b588a717920e
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-02-09 21:03:30 +0100

    Remove unused parameter in get_tree function

--- treefeeder/tree.py
+++ treefeeder/tree.py
@@ -41,7 +41,6 @@ def get_tree(directory, pattern=None, ignore_pattern=None):
     Get the tree representation of the directory.
 
     :param directory: The directory to list
-    :param prefix: The prefix to use for the tree
     :param pattern: List only those files that match the pattern
     :param ignore_pattern: Do not list files that match the given pattern
     :return: The tree representation of the directory
