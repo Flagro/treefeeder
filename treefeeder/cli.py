3ad2fd862db1fbb5a6c180d99632a7a12b3e3949
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-02-09 21:21:59 +0100

    Update get_output function to include a separator between statements

--- treefeeder/cli.py
+++ treefeeder/cli.py
@@ -6,4 +6,5 @@ def get_args():
     parser.add_argument('directory', nargs='?', default='.', help='Directory to start listing')
     parser.add_argument('-P', '--pattern', action='append', help='List only those files that match the pattern')
     parser.add_argument('-I', '--ignore', action='append', help='Do not list files that match the given pattern')
+    parser.add_argument('-S', '--separator', default='[SEP]', help='Separator to use between statements')
     return parser.parse_args()
--- treefeeder/printer.py
+++ treefeeder/printer.py
@@ -2,7 +2,10 @@ from .tree import get_tree
 from .contents import get_contents
 
 
-def get_output(directory, prefix, pattern=None, ignore_pattern=None):
-    tree_output = get_tree(directory, prefix, pattern, ignore_pattern)
+def get_output(directory, pattern=None, ignore_pattern=None, separator=None):
+    tree_output = get_tree(directory, pattern, ignore_pattern)
     contents_output = get_contents(directory, pattern, ignore_pattern)
-    return tree_output + contents_output
+    
+    output = (separator + "\n").join([tree_output] + contents_output + ["User's query:"])
+    
+    return output
